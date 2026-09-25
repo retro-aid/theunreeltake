db5def5648568223d4718a1168f86d0fd7ecf370
Izak Puente Zavala <puenteizak@live.com>
2026-09-25 16:47:30 -0700

    Filters, Sort, and refresh completed. Was merged with URT374 beacuse seachbar is on same page

--- src/lib/actions/post-actions.ts
+++ src/lib/actions/post-actions.ts
@@ -1,9 +1,15 @@
 "use server";
 
-import {updatePost, UpdatePostArgs} from "@/lib/dal/dto/posts";
+import { getAdminPosts, updatePost, UpdatePostArgs } from "@/lib/dal/dto/posts";
 
 export async function updatePostAction(
   updatePostArgs: UpdatePostArgs
 ) {
   return updatePost(updatePostArgs);
+}
+
+export async function getAdminPostsAction(
+  query: Parameters<typeof getAdminPosts>[0]
+) {
+  return getAdminPosts(query);
 }
